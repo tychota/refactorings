d4d49918842d174d46984d5df91f19e0bc77e0bf
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2019-02-12 18:57:36 +0100

    :sparkles: (split loop (227)) split perf loop into 2 loops

--- src/statement.ts
+++ src/statement.ts
@@ -5,11 +5,12 @@ export function statement(invoice: Invoice, plays: PlaysMap) {
   let result = `Statement for ${invoice.customer} \n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
+  }
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
   }
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
