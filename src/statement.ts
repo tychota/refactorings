dbb9ab35bb2df1e2c3dc7a2a7fc0b5781077a012
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2019-02-12 19:07:27 +0100

    :sparkles: (extract function (106)) remove totalAmount variable in the same way

--- src/statement.ts
+++ src/statement.ts
@@ -1,17 +1,22 @@
 export function statement(invoice: Invoice, plays: PlaysMap) {
-  let totalAmount = 0;
-
   let result = `Statement for ${invoice.customer} \n`;
 
   for (let perf of invoice.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits \n`;
   return result;
 
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      // print line for this order
+      result += amountFor(perf);
+    }
+    return result;
+  }
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of invoice.performances) {
