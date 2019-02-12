991ba2924a09c4a4da322978e76be801baa88441
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2019-02-12 20:01:28 +0100

    :sparkles: (html) add html renderer

--- src/statement.ts
+++ src/statement.ts
@@ -4,21 +4,34 @@ import { createStatementData } from "./createStatementData";
 export function statement(invoice: Invoice, plays: PlaysMap) {
   return renderPlainText(createStatementData(invoice, plays));
 }
-
 function renderPlainText(data: Data) {
-  let result = `Statement for ${data.customer} \n`;
+  let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
     result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits \n`;
   return result;
-
-  function usd(aNumber: number) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber / 100);
+}
+export function htmlStatement(invoice: Invoice, plays: PlaysMap) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+function renderHtml(data: Data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += `<table>`;
+  result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>`;
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>${usd(perf.audience)}</td><td>${perf.amount}<td>\n`;
   }
+  result += `</table>\n`;
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+function usd(aNumber: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber / 100);
 }
--- tests/statement.test.ts
+++ tests/statement.test.ts
@@ -1,4 +1,4 @@
-import { statement } from "../src/statement";
+import { statement, htmlStatement } from "../src/statement";
 
 const plays: PlaysMap = {
   hamlet: {
@@ -26,7 +26,7 @@ const invoices: Invoice[] = [
   }
 ];
 
-test("statement should output", () => {
+test("statement should output text", () => {
   // given
   const invoice = invoices[0];
 
@@ -34,12 +34,31 @@ test("statement should output", () => {
   const text = statement(invoice, plays);
 
   // then
-  const expectedTest = `Statement for BigCo 
+  const expectedTest = `Statement for BigCo
   Hamlet: $650.00 (55 seats)
   As You Like It: $580.00 (35 seats)
   Othello: $500.00 (40 seats)
 Amount owed is $1,730.00
 You earned 47 credits 
+`;
+  expect(text).toBe(expectedTest);
+});
+
+test("htmlStatement should output html", () => {
+  // given
+  const invoice = invoices[0];
+
+  // when
+  const text = htmlStatement(invoice, plays);
+
+  // then
+  const expectedTest = `<h1>Statement for BigCo</h1>
+<table><tr><th>play</th><th>seats</th><th>cost</th></tr><tr><td>Hamlet</td><td>$0.55</td><td>65000<td>
+<tr><td>As You Like It</td><td>$0.35</td><td>58000<td>
+<tr><td>Othello</td><td>$0.40</td><td>50000<td>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
 `;
   expect(text).toBe(expectedTest);
 });
