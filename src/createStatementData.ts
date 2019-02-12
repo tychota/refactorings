8586682acbcad0872451719158e89e5c4365f907
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2019-02-12 20:11:43 +0100

    :sparkles: (create perfomance calculato and replace amountFor by its usage)

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -9,8 +9,9 @@ export function createStatementData(invoice: Invoice, plays: PlaysMap) {
   return statementData as Data;
 
   function enrichPerformance(aPerformance: Perfomance): EnrichedPerfomance {
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance as EnrichedPerfomance));
     const result: Partial<EnrichedPerfomance> = { ...aPerformance };
-    result.play = playFor(result as EnrichedPerfomance);
+    result.play = calculator.play;
     result.amount = amountFor(result as EnrichedPerfomance);
     result.volumeCredits = volumeCreditsFor(result as EnrichedPerfomance);
     return result as EnrichedPerfomance;
@@ -25,31 +26,46 @@ export function createStatementData(invoice: Invoice, plays: PlaysMap) {
     return plays[aPerformance.playID];
   }
   function amountFor(aPerformance: EnrichedPerfomance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
   function volumeCreditsFor(aPerformance: EnrichedPerfomance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
     if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+}
+class PerformanceCalculator {
+  constructor(private _performance: Perfomance, private _play: Play) {}
+
+  public get play() {
+    return this._play;
+  }
+
+  public get performance() {
+    return this._performance;
+  }
+
+  public get amount() {
+    let result = 0;
+
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
     return result;
   }
 }
