a03c752d48ea5b07bd1343fe1bf532e0cec46e56
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2019-02-12 18:58:11 +0100

    :sparkles: (slide statement (223)) slide volumeCredits

--- src/statement.ts
+++ src/statement.ts
@@ -1,6 +1,5 @@
 export function statement(invoice: Invoice, plays: PlaysMap) {
   let totalAmount = 0;
-  let volumeCredits = 0;
 
   let result = `Statement for ${invoice.customer} \n`;
 
@@ -9,6 +8,7 @@ export function statement(invoice: Invoice, plays: PlaysMap) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
