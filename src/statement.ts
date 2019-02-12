69b4300b4a226b7da07008aa9af527cb75016c1a
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2019-02-12 18:54:25 +0100

    :sparkles: (change function declaration (124))

--- src/statement.ts
+++ src/statement.ts
@@ -8,19 +8,19 @@ export function statement(invoice: Invoice, plays: PlaysMap) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
   return result;
 
-  function format(aNumber: number) {
+  function usd(aNumber: number) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
   function volumeCreditsFor(aPerformance: Perfomance) {
     let result = 0;
